9d17507a541940ef5d625d96229a6ed0872c486f
Vairamuthu143 <117798425+Vairamuthu143@users.noreply.github.com>
2023-12-08 19:46:06 +0530

    Add files via upload

---
+++ gunaa1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int i=0,k=0;
+{
+	gets(a);
+while[x[i]!="\0"]
+{
+	i++
+}
+if k==j
+{
+	printf("palindrone");
+	for(j=0;j<i-1;i++)
+}
+if(x[j]==x[i-1])
+{
+	k++
+}
+else
+printf("not palindrome");
+}
